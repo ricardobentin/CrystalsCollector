cc7cf70b9cec5bac87cce130392e8a2e6d65921a
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-26 19:27:39 -0400

    removed console.log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,11 +18,6 @@ function startGame() {
     var greenNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
     var orangeNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
     var pinkNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-    //console.log the numbers to show them for now
-    console.log("blueNumber is: " + blueNumber);
-    console.log("greenNumber is: " + greenNumber);
-    console.log("orangeNumber is: " + orangeNumber);
-    console.log("pinkNumber is: " + pinkNumber);
     //logic for appending the value of each number to an attrbute named value.
     $("#blue").attr("value", blueNumber);
     $("#green").attr("value", greenNumber);
