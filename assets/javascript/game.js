ff9c21316cf9bc7ed7517aa82e7851c60b0fff97
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-26 14:41:35 -0400

    optimized my code with functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,39 @@
-var userScore = 0;
+//decalre necessary variables
+var userScore;
 var wins = 0;
 var losses = 0;
+var computerScore;
+//create a function to initialize and clear the game during each win or loss event
+function startGame() {
+    //clear user score
+    userScore = 0;
+    //print new user score to page
+    $("#userScore").text(userScore);
+    //generate a random number between 19 and 120 for the computerScore
+    computerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    //show computerScore on the screen
+    $("#computerScore").text(computerScore);
+    //generate a random number between 1 and 12 for the gems
+    var blueNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    var greenNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    var orangeNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    var pinkNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    //console.log the numbers to show them for now
+    console.log("blueNumber is: " + blueNumber);
+    console.log("greenNumber is: " + greenNumber);
+    console.log("orangeNumber is: " + orangeNumber);
+    console.log("pinkNumber is: " + pinkNumber);
+    //logic for appending the value of each number to an attrbute named value.
+    $("#blue").attr("value", blueNumber);
+    $("#green").attr("value", greenNumber);
+    $("#orange").attr("value", orangeNumber);
+    $("#pink").attr("value", pinkNumber);
+}
 //print wins, losses, and userScore to start the game.
 $("#wins").text("Wins: " + wins);
 $("#losses").text("Losses: " + losses);
 $("#userScore").text(userScore);
-//generate a random number between 19 and 120 for the computerScore
-var computerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-//show computerScore on the screen
-$("#computerScore").text(computerScore);
-//generate a random number between 1 and 12 for the gems
-var blueNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-var greenNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-var orangeNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-var pinkNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-//console.log the numbers to show them for now
-console.log("blueNumber is: " + blueNumber);
-console.log("greenNumber is: " + greenNumber);
-console.log("orangeNumber is: " + orangeNumber);
-console.log("pinkNumber is: " + pinkNumber);
-//logic for appending the value of each number to an attrbute named value.
-$("#blue").attr("value", blueNumber);
-$("#green").attr("value", greenNumber);
-$("#orange").attr("value", orangeNumber);
-$("#pink").attr("value", pinkNumber);
+
 //logic for adding the value of each gem to the user's score
 $("img").click(function () {
     //this is how to add each of the gem's value's to the user's score
@@ -36,57 +46,15 @@ $("img").click(function () {
         $("#message").text("You Won!");
         $("#wins").text("Wins: " + wins);
         //reset the game
-        //clear user score
-        userScore = 0;
-        //print new user score to page
-        $("#userScore").text(userScore);
-        //generate a new number for computer score
-        computerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-        //show computerScore on the screen
-        $("#computerScore").text(computerScore);
-        //generate a new random number between 1 and 12 for the gems
-        blueNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-        greenNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-        orangeNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-        pinkNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-        //console.log the numbers to show them for now
-        console.log("blueNumber is: " + blueNumber);
-        console.log("greenNumber is: " + greenNumber);
-        console.log("orangeNumber is: " + orangeNumber);
-        console.log("pinkNumber is: " + pinkNumber);
-        //logic for appending the value of each number to an attrbute named value.
-        $("#blue").attr("value", blueNumber);
-        $("#green").attr("value", greenNumber);
-        $("#orange").attr("value", orangeNumber);
-        $("#pink").attr("value", pinkNumber);
+        startGame();
     }
     else if (userScore > computerScore) {
         losses++;
         $("#message").text("You Lost!");
         $("#losses").text("Losses: " + losses);
-                //reset the game
-        //clear user score
-        userScore = 0;
-        //print new user score to page
-        $("#userScore").text(userScore);
-        //generate a new number for computer score
-        computerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-        //show computerScore on the screen
-        $("#computerScore").text(computerScore);
-        //generate a new random number between 1 and 12 for the gems
-        blueNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-        greenNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-        orangeNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-        pinkNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-        //console.log the numbers to show them for now
-        console.log("blueNumber is: " + blueNumber);
-        console.log("greenNumber is: " + greenNumber);
-        console.log("orangeNumber is: " + orangeNumber);
-        console.log("pinkNumber is: " + pinkNumber);
-        //logic for appending the value of each number to an attrbute named value.
-        $("#blue").attr("value", blueNumber);
-        $("#green").attr("value", greenNumber);
-        $("#orange").attr("value", orangeNumber);
-        $("#pink").attr("value", pinkNumber);
+        //reset the game
+        startGame();
     }
 });
+//function to call start game to initialize the game for the first time
+startGame();
